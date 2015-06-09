a7086759f4beed742408428c99d280ca4fc3b1c6
Shai Pe'er <shaip86@gmail.com>
2015-06-09 09:11:04 +0300

    Calendar date list generator

--- Attender/app/src/main/java/com/attender/CalendarPageActivity.java
+++ Attender/app/src/main/java/com/attender/CalendarPageActivity.java
@@ -19,8 +19,10 @@ import android.widget.Toast;
 
 import com.facebook.AccessToken;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 //import static com.example.rita.attender.R.id.listView;
@@ -30,6 +32,7 @@ public class CalendarPageActivity extends Activity
     private AttenderBL bl;
     private GridView calendarGrid;
     private ArrayList<Event> userEvents;
+    Calendar calendar = Calendar.getInstance();
 
 
 
@@ -56,11 +59,9 @@ public class CalendarPageActivity extends Activity
         Event_TV.setTypeface(tf);
 
 
-        //=============================  load date  ======================================
+        //=============================  Event List  ======================================
 
         final ListView listView = (ListView) findViewById(R.id.listView);
-        calendarGrid = (GridView) findViewById(R.id.calendar_grid);
-
 
         listView.setOnItemClickListener( new AdapterView.OnItemClickListener()
         {
@@ -76,23 +77,8 @@ public class CalendarPageActivity extends Activity
             }
         });
 
-
-
-        //=================================================================================================
-        //=================================================================================================
-        //=================================================================================================
-        //=================================================================================================
-
-        createGrid();
-
-
-
-
-
-        //=================================================================================================
-        //=================================================================================================
-        //=================================================================================================
-        //=================================================================================================
+        //============================  Calendar Grid =============================================
+        refreshCalendarGrid(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
 
   /*      myCal.setOnDateChangeListener(new CalendarView.OnDateChangeListener()
         {
@@ -124,15 +110,18 @@ public class CalendarPageActivity extends Activity
             }
         });
 */
+
     }
 
-    private void createGrid()
+    private void refreshCalendarGrid(int year, int mounth, int day)
     {
         ArrayList<String> items = new ArrayList<>();
-        /*String items[];
-        = { "1", "is", "a", "really", "really2", "really3",
-                "really4", "really5", "silly", "list" };
-*/
+
+        mounth += 1; // Jan = 0, Dec = 11
+
+        //SimpleDateFormat sdf = new SimpleDateFormat("yyyy MMM dd HH:mm:ss");
+        Calendar calendar = new GregorianCalendar(2014,9,14,12,51,56);
+
         for(int i = 1 ; i <= 43 ; i++)
         {
             items.add("" + i);
