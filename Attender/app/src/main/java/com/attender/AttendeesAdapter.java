999b9d5e71f86076eb0a1139adbbe9d032a52ab5
itamar sharify <itamarsharify@gmail.com>
2015-06-17 13:34:27 +0300

    stating to fix Fb friends
    working on issue #39
    closed #41

--- Attender/app/src/main/java/com/attender/AttendeesAdapter.java
+++ Attender/app/src/main/java/com/attender/AttendeesAdapter.java
@@ -6,9 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 public class AttendeesAdapter extends ArrayAdapter<String>
 {
@@ -41,7 +44,11 @@ public class AttendeesAdapter extends ArrayAdapter<String>
         else
             ((TextView) view.findViewById(R.id.tvCity)).setText("good man");
         if(at.is_ff())
+        {
+            ImageView iv = new ImageView(getApplicationContext());
+            iv.setImageResource(R.drawable.facebook_friend);
             ((TextView) view.findViewById(R.id.tvDate)).setText("friend");
+        }
         else
             ((TextView) view.findViewById(R.id.tvDate)).setText("not friend");
 
